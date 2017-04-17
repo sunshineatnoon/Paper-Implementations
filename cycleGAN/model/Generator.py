27fda4527800f9bed0f95684dd860126f21abe06
lixueting <lixueting@lixuetingdeMacBook-Pro.local>
2017-04-17 21:03:36 +0800

    cycleGAN

---
+++ cycleGAN/model/Generator.py
@@ -0,0 +1,107 @@
+import torch.nn as nn
+
+# courtesy: https://github.com/darkstar112358/fast-neural-style/blob/master/neural_style/transformer_net.py
+class InstanceNormalization(torch.nn.Module):
+    """InstanceNormalization
+    Improves convergence of neural-style.
+    ref: https://arxiv.org/pdf/1607.08022.pdf
+    """
+
+    def __init__(self, dim, eps=1e-9):
+        super(InstanceNormalization, self).__init__()
+        self.scale = nn.Parameter(torch.FloatTensor(dim))
+        self.shift = nn.Parameter(torch.FloatTensor(dim))
+        self.eps = eps
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        self.scale.data.uniform_()
+        self.shift.data.zero_()
+
+    def forward(self, x):
+        n = x.size(2) * x.size(3)
+        t = x.view(x.size(0), x.size(1), n)
+        mean = torch.mean(t, 2).unsqueeze(2).expand_as(x)
+        # Calculate the biased var. torch.var returns unbiased var
+        var = torch.var(t, 2).unsqueeze(2).expand_as(x) * ((n - 1) / float(n))
+        scale_broadcast = self.scale.unsqueeze(1).unsqueeze(1).unsqueeze(0)
+        scale_broadcast = scale_broadcast.expand_as(x)
+        shift_broadcast = self.shift.unsqueeze(1).unsqueeze(1).unsqueeze(0)
+        shift_broadcast = shift_broadcast.expand_as(x)
+        out = (x - mean) / torch.sqrt(var + self.eps)
+        out = out * scale_broadcast + shift_broadcast
+        return out
+
+def build_conv_block(dim):
+    return nn.Sequential(nn.ReflectionPad2d((1,1,1,1)),
+                         nn.Conv2d(dim,dim,3,3,1,1,0,0),
+                         InstanceNormalization(dim),
+                         nn.ReLU(True),
+                         nn.ReflectionPad2d((1,1,1,1)),
+                         nn.Conv2d(dim,dim,3,3,1,1,0,0),
+                         InstanceNormalization(dim))
+
+class ResidualBlock(nn.Module):
+    '''
+    A single Residual Block:
+    in ->
+        conv(in_channels,out_channels,stride) -> BN -> ReLU
+        conv(out_channels,out_channels,1) -> BN
+    -> out
+    (downsample)in + out
+    '''
+    def __init__(self, dim):
+        super(ResidualBlock,self).__init__()
+        self.conv = build_conv_block(dim)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = out + x
+
+        return out
+
+class Generator(nn.Module):
+    def __init__(self, input_nc, output_nc, ngf):
+        super(Generator,self).__init__()
+        # 128 x 128
+        self.layer1 = nn.Sequential(nn.ReflectionPad2d(3,3,3,3),
+                                    nn.Conv2d(input_nc,ngf,kernel_size=7,stride=1),
+                                    InstanceNormalization(ngf),
+                                    nn.ReLU(True))
+        # 128 x 128
+        self.layer2 = nn.Sequential(nn.Conv2d(ngf,ngf*2,kernel_size=3,stride=2,padding=1),
+                                   InstanceNormalization(ngf*2),
+                                   nn.ReLU(True))
+        # 64 x 64
+        self.layer3 = nn.Sequential(nn.Conv2d(ngf*2,ngf*4,kernel_size=3,stride=2,padding=1),
+                                   InstanceNormalization(ngf*4),
+                                   nn.ReLU(True))
+        # 32 x 32
+        self.layer4 = nn.Sequential(ResidualBlock(ngf*4),
+                                    ResidualBlock(ngf*4),
+                                    ResidualBlock(ngf*4),
+                                    ResidualBlock(ngf*4),
+                                    ResidualBlock(ngf*4),
+                                    ResidualBlock(ngf*4))
+        # 32 x 32
+        self.layer5 = nn.Sequential(nn.ConvTranspose2d(ngf*4, ngf*2, 3, 2, 1, 1),
+                                     InstanceNormalization(ngf*2),
+                                     nn.ReLU(True))
+        # 64 x 64
+        self.layer6 = nn.Sequential(nn.ConvTranspose2d(ngf*2, ngf, 3, 2, 1, 1),
+                                     nn.Conv2d(ngf,output_nc,kernel_size=7,stride=1),
+                                     nn.ReLU(True))
+        # 128 x 128
+        self.layer7 = nn.Sequential(nn.ReflectionPad2d((3,3,3,3)),
+                                     nn.Conv2d(ngf,output_nc,kernel_size=7,stride=1),
+                                     nn.Tanh())
+        # 128 x 128
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        return out
---
+++ cycleGAN/utils/dataset.py
@@ -0,0 +1,86 @@
+import torch
+import numpy as np
+import torch.utils.data as data
+from os import listdir
+from os.path import join
+from utils import is_image_file
+import os
+from PIL import Image
+import random
+
+def default_loader(path):
+    return Image.open(path).convert('RGB')
+
+def ToTensor(pic):
+    """Converts a PIL.Image or numpy.ndarray (H x W x C) in the range
+    [0, 255] to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0].
+    """
+    if isinstance(pic, np.ndarray):
+        # handle numpy array
+        img = torch.from_numpy(pic.transpose((2, 0, 1)))
+        # backard compability
+        return img.float().div(255)
+    # handle PIL Image
+    if pic.mode == 'I':
+        img = torch.from_numpy(np.array(pic, np.int32, copy=False))
+    elif pic.mode == 'I;16':
+        img = torch.from_numpy(np.array(pic, np.int16, copy=False))
+    else:
+        img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
+    # PIL image mode: 1, L, P, I, F, RGB, YCbCr, RGBA, CMYK
+    if pic.mode == 'YCbCr':
+        nchannel = 3
+    elif pic.mode == 'I;16':
+        nchannel = 1
+    else:
+        nchannel = len(pic.mode)
+    img = img.view(pic.size[1], pic.size[0], nchannel)
+    # put it from HWC to CHW format
+    # yikes, this transpose takes 80% of the loading time/CPU
+    img = img.transpose(0, 1).transpose(0, 2).contiguous()
+    if isinstance(img, torch.ByteTensor):
+        return img.float().div(255)
+    else:
+        return img
+
+
+# You should build custom dataset as below.
+class DATASET(data.Dataset):
+    def __init__(self,dataPath='',loadSize=72,fineSize=64,flip=1):
+        super(DATASET, self).__init__()
+        # list all images into a list
+        self.list = [x for x in listdir(dataPath) if is_image_file(x)]
+        self.dataPath = dataPath
+        self.loadSize = loadSize
+        self.fineSize = fineSize
+        self.flip = flip
+
+    def __getitem__(self, index):
+        # 1. Read one data from file (e.g. using numpy.fromfile, PIL.Image.open).
+        path = os.path.join(self.dataPath,self.list[index])
+        img = default_loader(path) # 256x256
+
+        # 2. seperate image A and B; Scale; Random Crop; to Tensor
+        w,h = img.size
+
+        if(h != self.loadSize):
+            img = img.resize((self.loadSize, self.loadSize), Image.BILINEAR)
+
+        if(self.loadSize != self.fineSize):
+            x1 = random.randint(0, self.loadSize - self.fineSize)
+            y1 = random.randint(0, self.loadSize - self.fineSize)
+            img = img.crop((x1, y1, x1 + self.fineSize, y1 + self.fineSize))
+
+        if(self.flip == 1):
+            if random.random() < 0.5:
+                img = img.transpose(Image.FLIP_LEFT_RIGHT)
+
+        img = ToTensor(img) # 3 x 256 x 256
+
+        img = img.mul_(2).add_(-1)
+        # 3. Return a data pair (e.g. image and label).
+        return img
+
+    def __len__(self):
+        # You should change 0 to the total size of your dataset.
+        return len(self.list)
